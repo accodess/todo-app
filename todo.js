dd395930d2f6589c9b50929f004cde7806113f37
Alona <alona.costa2@gmail.com>
2022-11-30 20:44:06 +0000

    localSrorage, refactoring

--- todo.js
+++ todo.js
@@ -1,46 +1,8 @@
-const todos = [
-  {
-    text: "Buy food",
-    completed: true,
-  },
-  {
-    text: "Clean house",
-    completed: false,
-  },
-  {
-    text: "Do exercise",
-    completed: false,
-  },
-  {
-    text: "Finish book",
-    completed: false,
-  },
-];
+const todos = getSavedTodos();
 
 const filters = {
   searchText: "",
-};
-
-const renderTodos = function (todos, filters) {
-  const filteredTodos = todos.filter(function (todo) {
-    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-  });
-
-  const incompleteTodos = filteredTodos.filter(function (todo) {
-    return !todo.completed;
-  });
-
-  document.querySelector('#todos').innerHTML = ''
-
-  const summary = document.createElement("h2");
-  summary.textContent = `You have ${incompleteTodos.length} todos left`;
-  document.querySelector("#todos").appendChild(summary);  
-
-  filteredTodos.forEach(function (todo) {
-    const p = document.createElement("p");
-    p.textContent = todo.text;
-    document.querySelector("#todos").appendChild(p);
-  });
+  hideCompleted: "",
 };
 
 renderTodos(todos, filters);
@@ -50,12 +12,18 @@ document.querySelector("#search-todo").addEventListener("input", function (e) {
   renderTodos(todos, filters);
 });
 
-document.querySelector('#todo-form').addEventListener('submit', function (e) {
-  e.preventDefault()
+document.querySelector("#todo-form").addEventListener("submit", function (e) {
+  e.preventDefault();
   todos.push({
     text: e.target.elements.todoName.value,
-    completed: false
-  }) 
-  renderTodos(todos, filters)
-  e.target.elements.todoName.value = ''
-})
+    completed: false,
+  });
+  saveTodos(todos);
+  renderTodos(todos, filters);
+  e.target.elements.todoName.value = "";
+});
+
+document.querySelector("#hide-todo").addEventListener("change", function (e) {
+  filters.hideCompleted = e.target.checked;
+  renderTodos(todos, filters);
+});
